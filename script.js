833926a962a79539b7a0e90c9e920212b54135af
OU-Tank <uren0464@student.acitech.org>
2025-03-03 09:14:12 -0500

    I'm basically done with my red button.

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-const button1 = document.getElementById("button1");
+let text = document.getElementById( "text" )
+let button1 = document.getElementById("button1");
 
-button1.addEventListener("click", yell_at_user)
+let clicks= 0;
 
-function yell_at_user() {
-    alert("STOP PRESSING ME")
-}
+button1.addEventListener("click", annoy)
+
+function annoy() {
+    clicks++;
+  if(clicks == 1){
+    text.innerHTML = "STOP PRESSING MEEEEEEEEEEEEEE!!!!"
+  }
+  else if(clicks == 4 ){
+    text.innerHTML = "I AM GETTING ANGRY!!!!!"
+  }
+  else if(clicks == 6){
+    text.innerHTML = "YOU'RE GOING TO UNLEASH THE BEAST!!!"
+  }
+    else if(clicks == 8 ){
+        text.innerHTML = "STOOOOOOOOOOOPPPPPPPPPPP!!!!!"
+      }
+      else if(clicks == 10 ){
+        text.innerHTML = "THAT'S IT, I'M COMING TO YOUR HOUSE"
+      }
+      else if(clicks == 12){
+        text.innerHTML = "100 METERS"
+      }
+        else if(clicks == 14 ){
+            text.innerHTML = "60 METERS"
+          } 
+          else if(clicks == 16 ){
+            text.innerHTML = "40 METERS"
+          }
+
+          else if(clicks == 18 ){
+            text.innerHTML = "LOOK OUTSIDE"
+          }
+      }
+  
